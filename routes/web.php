098e472df346685e5379f2f30f113b434c4c43ad
dimadani <dikia656@gmail.com>
2022-07-15 22:31:33 +0700

    feat(input_attendance): create new page input attendance

--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,12 @@ Route::get('/', function () {
 
 Route::get('/user', fn () => view('user.dashboard'));
 Route::get('/user/edit-profil', fn () => view('user.edit_profil'));
-Route::get('/kadiv/edit-profil', fn () => view('kadiv.edit_profil'));
+Route::get('/user/input_absensi', fn () => view('user.input_absensi'));
+
 Route::get('/kadiv', fn () => view('kadiv.dashboard'));
-Route::get('/admin/edit-profil', fn () => view('admin.edit_profil'));
+Route::get('/kadiv/edit-profil', fn () => view('kadiv.edit_profil'));
+
 Route::get('/admin/add-user', fn () => view('admin.add_user'));
+Route::get('/admin/edit-profil', fn () => view('admin.edit_profil'));
 
 Route::resource('/presence', PresenceController::class);
