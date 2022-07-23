5bad57b71a5874c112c904fb0d982ef60756019e
Versa Feodora <2141720156@student.polinema.ac.id>
2022-07-23 18:00:02 +0700

    Meetings - API

---
+++ app/Http/Controllers/Api/MeetingsController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use \App\Http\Requests\MeetingsRequest;
+use \App\Models\Meetings;
+use \Illuminate\Http\Response;
+use App\Http\Controller;
+
+class MeetingsController extends Controller
+{
+        /**
+     * Display a listing of the resource.
+     *
+     * 
+     */
+    public function index()
+    {
+        return response()->json([
+            'response' => Meetings::all()
+        ]);
+    }
+    
+    public function edit(Meetings $meeting)
+    {
+        return response()->json([
+            'response' => $meeting
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     *
+     */
+    public function store(MeetingsRequest $request)
+    {
+        $data = $request->validate();
+        Meetings::create($data);
+        return response()->json([
+            'response' => $data
+        ]);
+    }
+
+
+
+    /**
+     * Display the specified resource.
+     *
+     *
+     */
+    public function show(Meetings $meeting){
+        return response()->json([
+            'response' => $meeting
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * 
+     */
+    public function update(MeetingsResponse $request, Meetings $meeting)
+    {
+        $newData = $request->validate();
+        $data = Meetings::where('id', $meeting->id)->update($newData);
+        return response()->json([
+            'response' => $newData
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     *
+     */
+    public function destroy(Meetings $meeting)
+    {
+        Meetings::destroy($meeting->id);
+        return response()->json([
+            'response' => 'success'
+        ]);
+    }
+}
