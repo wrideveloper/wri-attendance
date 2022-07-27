c2cf33d5a06f3812d434b5e86cb34aec123d18c3
Agilar011 <88954314+Agilar011@users.noreply.github.com>
2022-07-27 18:32:28 +0700

    File model user

---
+++ app/Models/User.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $guarded = [
+        'id',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function roles()
+    {
+        return $this->belongsTo(Roles::class);
+    }
+
+    public function presence()
+    {
+        return $this->hasMany(Presence::class, 'nim', 'nim');
+    }
+
+    public function hadir()
+    {
+        return $this->presence()->where('status', 'Hadir')->orderBy('id', 'desc');
+    }
+
+    public function izin()
+    {
+        return $this->presence()->where('status', 'Izin')->orderBy('id', 'desc');
+    }
+
+    public function sakit()
+    {
+        return $this->presence()->where('status', 'Sakit')->orderBy('id', 'desc');
+    }
+
+    public function alpha()
+    {
+        return $this->presence()->where('status', 'Alpha')->orderBy('id', 'desc');
+    }
+}
