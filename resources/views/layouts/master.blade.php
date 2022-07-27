42a6a2e6c38f91fd5079476b024cfd90c01b07c6
aliyyaps <aliyyaputris23@gmail.com>
2022-07-27 18:25:48 +0700

    Membuat Responsive

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -17,6 +17,7 @@
     <link rel="stylesheet" href="{{ asset('css/input-absensi.css') }}">
     <link rel="stylesheet" href="{{ asset('css/edit_absensi.css') }}">
     <link rel="stylesheet" href="{{ asset('css/sidebar.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/update_jadwal.css') }}">
     <title>WRI Attendance | Sistem Absensi Miniclass</title>
 
 </head>
@@ -25,21 +26,16 @@
     <div class="d-flex" id="wrapper">
         <div class="sidebar" id="sidebar-wrapper">
             <div class="sidebar-brand">
-                <a href="" class=""><img src="{{ asset('img/wri-logo-white.png') }}"
-                        alt="wri-polinema"></a>
+                <a href="" class=""><img src="{{ asset('img/wri-logo-white.png') }}" alt="wri-polinema"></a>
             </div>
             <div class="list-group rounded-0">
-                <a class="list-group-item d-inline-block text-truncate active" href="#!"><i
-                        class="fa-solid fa-border-all" style="padding-right: 12px;"></i>
+                <a class="list-group-item d-inline-block text-truncate active" href="#!"><i class="fa-solid fa-border-all" style="padding-right: 12px;"></i>
                     <span>Dashboard</span></a>
-                <a class="list-group-item d-inline-block text-truncate" href="#!"><i class="fa-solid fa-user"
-                        style="padding-right: 12px;"></i>
+                <a class="list-group-item d-inline-block text-truncate" href="#!"><i class="fa-solid fa-user" style="padding-right: 12px;"></i>
                     <span>Crud User</span></a>
-                <a class="list-group-item d-inline-block text-truncate" href="#!"><i
-                        class="fa-solid fa-clipboard-user" style="padding-right: 14px;"></i><span>Update
+                <a class="list-group-item d-inline-block text-truncate" href="#!"><i class="fa-solid fa-clipboard-user" style="padding-right: 14px;"></i><span>Update
                         Absensi</span></a>
-                <a class="list-group-item d-inline-block text-truncate text-hover-red" href="#!"><i
-                        class="fa-solid fa-arrow-right-from-bracket" style="padding-right: 10px;"></i>
+                <a class="list-group-item d-inline-block text-truncate text-hover-red" href="#!"><i class="fa-solid fa-arrow-right-from-bracket" style="padding-right: 10px;"></i>
                     <span>Log
                         Out</span></a>
             </div>
@@ -50,16 +46,11 @@
                     <a class="btn-bar" id="sidebarToggle"><i class="fa-solid fa-bars"></i></a>
                     <ul class="navbar-nav">
                         <li class="nav-item dropdown ">
-                            <a class="nav-link position-relative" href="#" id="navbarDropdown" role="button"
-                                data-bs-toggle="dropdown" aria-expanded="false">
-                                <span class="text-sm-hidden">Dikhi Achmad Dani</span> <span class="item-users"><i
-                                        class="fa-solid fa-user"></i></span>
+                            <a class="nav-link position-relative" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <span class="text-sm-hidden">Dikhi Achmad Dani</span> <span class="item-users"><i class="fa-solid fa-user"></i></span>
                             </a>
-                            <ul class="dropdown-menu dropdown-menu-end my-2 position-absolute"
-                                aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item text-hover-red" href=""><i
-                                            class="fa-solid fa-arrow-right-from-bracket text-red"
-                                            style="padding-right: 10px;"></i>Logout</a> </li>
+                            <ul class="dropdown-menu dropdown-menu-end my-2 position-absolute" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item text-hover-red" href=""><i class="fa-solid fa-arrow-right-from-bracket text-red" style="padding-right: 10px;"></i>Logout</a> </li>
                             </ul>
                         </li>
                     </ul>
