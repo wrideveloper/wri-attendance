42f0efdf823633f88c45896f324f51bb0c561357
Zaed Abdullah <z4ed.thalib123@gmail.com>
2022-07-28 21:16:25 +0700

    Move 2 controller to API folder

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
