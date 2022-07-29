6ab420c1aae7597592c2a3081ced89e526cd5a5c
ilhamsg7 <sinatrio20@gmail.com>
2022-07-29 09:52:14 +0700

    Bring Back Auth Controller

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function index() {
+        return view('auth.login');
+    }
+
+    public function authenticate(Request $request)
+    {
+        $credential = $request->validate([
+            'nim' => 'required|exists:users,nim|string',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($credential)) {
+            $request->session()->regenerate();
+            return redirect()->intended('/dashboard');
+        }
+
+        return response()->json([
+            'success' => false,
+            'message' => 'Invalid Email or Password',
+        ], 401);
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+
+        request()->session()->invalidate();
+        request()->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
