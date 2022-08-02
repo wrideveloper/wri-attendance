ac11909af6f3c8d119dea910049e75f9a5d282dc
VersaFeodora <92081672+VersaFeodora@users.noreply.github.com>
2022-08-02 19:09:13 +0700

    Update MeetingsController.php

--- app/Http/Controllers/MeetingsController.php
+++ app/Http/Controllers/MeetingsController.php
@@ -17,7 +17,7 @@ class MeetingsController extends Controller {
     public function index()
     {
         $datas = Meetings::where('miniclass_id', Auth::user()->miniclass_id);
-        return view('kadiv.list_pertemuan', compact(datas));
+        return view('list-pertemuan', compact(datas));
     }
 
     public function create() {
