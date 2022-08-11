e07daf2c5005419c9f4ea2309f55bbdc761c8693
Agilar011 <88954314+Agilar011@users.noreply.github.com>
2022-08-11 09:51:35 +0700

    penambahan parameter

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -61,7 +62,7 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, User $user)
     {
         $validated = $request->validate([
             'miniclass_id' => 'required',
